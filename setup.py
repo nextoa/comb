3c8b7806e68bb326b1d9d478fe84e24b5fb0ebb0
Breeze.kay <dev@kbonez.net>
2014-06-02 09:02:31 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.8.4',
     packages=['comb', 'comb.mq', 'comb.demo'],
     scripts=['bin/comb'],
-    url='',
+    url='https://github.com/kbonez/comb',
     license='http://opensource.org/licenses/MIT',
     author='Breeze.Kay',
     author_email='wangwenpei@kbonez.com',
