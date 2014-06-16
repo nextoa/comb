d052827e0382d7ec4c856494a2c5841d79c27693
Breeze.kay <dev@kbonez.net>
2014-06-16 17:03:48 +0800

    update setup files

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 
 setup(
     name='comb',
-    version='0.8.12',
+    version='0.8.23',
     packages=['comb', 'comb.mq', 'comb.demo'],
     scripts=['bin/comb'],
+    install_requires=['cliez'],
     url='https://github.com/kbonez/comb',
     download_url = 'https://github.com/kbonez/comb/tarball/0.8.12',
     license='http://opensource.org/licenses/MIT',
